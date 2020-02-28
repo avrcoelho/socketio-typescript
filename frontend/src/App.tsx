52d77b9b379aaa3f7112096035bc67ef07183dee
aribeiro <aribeiro@nb-aribeiro.grupoiv2.local>
2020-02-28 10:52:44 -0300

    configura axios and getProducts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
+
+interface IProduct {
+  _id: string;
+  title: string;
+}
 
 const App: React.FC = () => {
+  const [products, setproducts] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const { data }: { data: IProduct[] } = await api.get('/products/?code=1');
+
+        setproducts(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getProducts();
+  })
+
   return (
     <h1>Hello world</h1>
   );
